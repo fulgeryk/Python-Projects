21189ac300cf85df5db2e17dded75a295b705e35
Fulger Sorin <fulgeryk_cs@yahoo.com>
2025-09-12 10:42:45 +0300

    Create snake and movement

---
+++ Day20/Snake.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+
+STARTING_POS = [0, -20, -40]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN= 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    def __init__(self):
+        self.snake = []
+        self.create_snake()
+        self.head = self.snake[0]
+
+    def create_snake(self):
+        for t in range(0, 3):
+            new_snake = Turtle(shape="square")
+            new_snake.color("white")
+            new_snake.penup()
+            new_snake.goto(STARTING_POS[t], 0)
+            self.snake.append(new_snake)
+
+    def move(self):
+        for seg_num in range(len(self.snake) - 1, 0, -1):
+            new_x = self.snake[seg_num - 1].xcor()
+            new_y = self.snake[seg_num - 1].ycor()
+            self.snake[seg_num].goto(new_x, new_y)
+        self.snake[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
