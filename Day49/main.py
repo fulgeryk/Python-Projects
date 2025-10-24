1ce1b03f6dbd311abb1a561ffd5b4887551ca120
Fulger Sorin <fulgeryk_cs@yahoo.com>
2025-10-24 13:07:14 +0300

    book_upcoming_tuesday

--- Day49/main.py
+++ Day49/main.py
@@ -2,3 +2,4 @@ from selenium_chrome import SeleniunConfigure
 
 new_web = SeleniunConfigure()
 new_web.login()
+new_web.book_upcoming_tuesday()
--- Day49/selenium_chrome.py
+++ Day49/selenium_chrome.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import os
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 ACCOUNT_EMAIL = "fulger@test.com"
 ACCOUNT_PASSWORD = "Logitech123"
@@ -26,4 +27,24 @@ class SeleniunConfigure:
         password_input.send_keys(ACCOUNT_PASSWORD)
         press_login = wait.until(lambda _: self.driver.find_element(By.ID, value="submit-button"))
         press_login.click()
+
+    def book_upcoming_tuesday(self):
+        wait = WebDriverWait(self.driver, 15)
+        wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div[id^='day-group-']")))
+        all_groups = self.driver.find_elements(By.CSS_SELECTOR, value="div[id^='day-group-']")
+        for card in all_groups:
+            day_title = card.find_element(By.CSS_SELECTOR, value = "h2[id^='day-title-']").text
+            if "Tue" in day_title:
+                time_tue = card.find_elements(By.CSS_SELECTOR, value = "p[id^='class-time-']")
+                for t in time_tue:
+                    if "6:00 PM" in t.text:
+                        click_button = card.find_element(By.CSS_SELECTOR, value = "button[id^='book-button-']")
+                        click_button.click()
+                        print(f"✓ Booked for {day_title} and at hour {t.text}")
+        #Just for debug
+        # see_booking = self.driver.find_element(By.ID, value="my-bookings-link")
+        # see_booking.click()
+
+
+
 
