6ee4c6912a863c81470c5c0584b369c06f4c00d9
Fulger Sorin <fulgeryk_cs@yahoo.com>
2025-10-24 13:47:49 +0300

    Implement Thu logic

--- Day49/selenium_chrome.py
+++ Day49/selenium_chrome.py
@@ -20,6 +20,7 @@ class SeleniunConfigure:
         self.CNT_BOOKED_JOINED = 0
         self.CNT_WAITLISTS_JOINED = 0
         self.CNT_ALREADY_BOOKED_OR_WAITLISTED = 0
+        self.summary = []
 
     def login(self):
         wait = WebDriverWait(self.driver, timeout=5)
@@ -38,25 +39,30 @@ class SeleniunConfigure:
         all_groups = self.driver.find_elements(By.CSS_SELECTOR, value="div[id^='day-group-']")
         for card in all_groups:
             day_title = card.find_element(By.CSS_SELECTOR, value = "h2[id^='day-title-']").text
-            if "Tue" in day_title:
+            if "Tue" in  day_title or "Thu" in day_title:
                 time_tue = card.find_elements(By.CSS_SELECTOR, value = "p[id^='class-time-']")
                 for t in time_tue:
                     if "6:00 PM" in t.text:
                         click_button = card.find_element(By.CSS_SELECTOR, value = "button[id^='book-button-']")
+                        class_name =  card.find_element(By.CSS_SELECTOR, value="h3[id^=class-name-]").text
                         if click_button.text == "Booked":
                             self.CNT_ALREADY_BOOKED_OR_WAITLISTED += 1
-                            print(f'✓ Already booked: {card.find_element(By.CSS_SELECTOR, value="h3[id^=class-name-]").text} on {day_title} at hout {t.text}')
+                            print(f'✓ Already booked: {class_name} on {day_title} at hout {t.text}')
+                            self.summary.append(f"• [Already Booked] {class_name} on {day_title} ({t.text})")
                         elif click_button.text == "Waitlisted":
                             self.CNT_ALREADY_BOOKED_OR_WAITLISTED += 1
-                            print(f'✓ Already on Waitlist: {card.find_element(By.CSS_SELECTOR, value="h3[id^=class-name-]").text} on {day_title} at hout {t.text}')
+                            print(f'✓ Already on Waitlist: {class_name} on {day_title} at hout {t.text}')
+                            self.summary.append(f"• [Already Waitlist] {class_name} on {day_title} ({t.text})")
                         elif click_button.text == "Join Waitlist":
                             self.CNT_WAITLISTS_JOINED += 1
                             click_button.click()
-                            print(f'✓ Joined Waitlist for: {card.find_element(By.CSS_SELECTOR, value="h3[id^=class-name-]").text} on {day_title} at hout {t.text}')
+                            print(f'✓ Joined Waitlist for: {class_name} on {day_title} at hout {t.text}')
+                            self.summary.append(f"• [New Waitlist] {class_name} on {day_title} ({t.text})")
                         else:
                             self.CNT_BOOKED_JOINED +=1
                             click_button.click()
-                            print(f"✓ Booked for {card.find_element(By.CSS_SELECTOR, value="h3[id^=class-name-]").text} on {day_title} and at hour {t.text}")
+                            print(f"✓ Booked for {class_name} on {day_title} and at hour {t.text}")
+                            self.summary.append(f"• [New Booking] {class_name} on {day_title} ({t.text})")
 
     def booking_summary(self):
         print("--- BOOKING SUMMARY ---")
@@ -64,7 +70,9 @@ class SeleniunConfigure:
         print(f"Waitlists joined: {self.CNT_WAITLISTS_JOINED}")
         print(f"Already Booked/waitlisted: {self.CNT_ALREADY_BOOKED_OR_WAITLISTED}")
         print(f"Total classes processed: {self.CNT_BOOKED_JOINED + self.CNT_WAITLISTS_JOINED + self.CNT_ALREADY_BOOKED_OR_WAITLISTED}")
-
+        print("\n--- DETAILED CLASS LIST ---")
+        for line in self.summary:
+            print(" ", line)
 
 
 
